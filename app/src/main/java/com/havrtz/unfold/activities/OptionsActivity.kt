ce42e216e653699567be1e2ec15db082ef0e64d7
Abhishek Jadhav <abhishek.jadhav64@gmail.com>
2020-08-28 16:44:54 +0530

    Updated contact me page

--- app/src/main/java/com/havrtz/unfold/activities/OptionsActivity.kt
+++ app/src/main/java/com/havrtz/unfold/activities/OptionsActivity.kt
@@ -1,8 +1,13 @@
 package com.havrtz.unfold.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ClipDescription
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.havrtz.unfold.R
+
 
 class OptionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,4 +32,32 @@ class OptionsActivity : AppCompatActivity() {
             }
         }
     }
+
+    fun openLinkedIn(view: View) {
+
+        var mLinkedInurlString: String = "abhishek-jadhav-36197898"
+
+        val url = "https://www.linkedin.com/in/$mLinkedInurlString"
+        val linkedInAppIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        linkedInAppIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET)
+        startActivity(linkedInAppIntent)
+    }
+
+    /** TODO: Only make it available on mail apps */
+    fun openMail(view: View) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = ClipDescription.MIMETYPE_TEXT_PLAIN
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("abhishek.jadhav64@live.com"))
+        intent.putExtra(android.content.Intent.EXTRA_SUBJECT,"")
+        intent.putExtra(android.content.Intent.EXTRA_TEXT, "")
+        startActivity(Intent.createChooser(intent,"Send Email"))
+    }
+
+    fun openGithub(view: View) {
+        val uri = Uri.parse("https://github.com/i-am-0xbit") // missing 'http://' will cause crashed
+
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        startActivity(intent)
+    }
+
 }
